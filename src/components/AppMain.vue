65ff09715c90e35703808e7d7c8246522c7260fe
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-17 16:45:00 +0100

    [add] card generate

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,11 @@
 <script>
 
 import { store } from '../data/store';
+import CharacterCard from './CharacterCard.vue'
 
 export default {
     name: 'AppMain',
+    components: { CharacterCard, },
     data(){
         return{
             store
@@ -13,11 +15,44 @@ export default {
 </script>
 
 <template>
-    
+    <main>
+        <div class="container">
+            <div class="row"></div>
+            <div class="col my-set">
+                <h4>Found XX characters</h4>
+            </div>
+            <div class="container-fluid d-flex flex-wrap justify-content-evenly">
+                <CharacterCard class="col-3 my-3"
+                v-for="character in store.characterListData"
+                :key="character.id"
+                :character="character"/>
+            </div>
+            
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
 
     @use '../styles/partials/variables.scss' as *;
+
+    .container{
+        padding: 30px;
+        border-radius: 30px;
+        background-color: rgba(0, 0, 0, .35);
+        min-height: 700px;
+        .col.my-set{
+            margin-bottom: 30px;
+            padding: 15px 0;
+            border-radius: 5px;
+            background-color: #333;
+            h4{
+                color: #fff;
+                font-weight: 500;
+                margin-left: 20px;
+                margin-bottom: 0;
+            }
+        }
+    }
 
 </style>
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name:'CharacterCard',
+    props: {
+        character: Object,
+    },
+}
+</script>
+
+<template>
+    <div class="card my-set" style="width: 18rem;">
+        <div class="avatar">
+            <img :src="character.img" class="card-img-top" :alt="character.name">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title text-center">{{character.name}}</h5>
+            <p class="card-text text-center">{{character.category}}</p>
+            <p class="card-text text-center">{{character.status}}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+    @use '../styles/partials/variables.scss' as *;
+
+    .card-body, .card{
+        border: none;
+        color: #fff;
+        background-color: $primary-color;
+        background-image: linear-gradient(0deg, $primary-color 0%, #85FFBD 90%);
+        box-shadow: 0 0 5px 1px rgba(0, 0, 0, .2);
+    }
+    .card{
+        filter: brightness(.8);
+        transition: all 400ms;
+        &:hover{
+            filter: brightness(1);
+            transform: scale(1.05);
+        }
+    }
+    .avatar{
+        max-height: 350px;
+    }
+    .card-title{
+        font-size: .9rem;
+        color: $primary-color;
+        margin-bottom: 20px;
+    }
+    .card-text{
+        font-size: .8rem;
+        margin-bottom: 5px;
+    }
+
+</style>
